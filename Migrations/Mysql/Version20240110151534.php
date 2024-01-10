ec89dfbb1bc0f16eb641d9a588c00b1b3ccbebf6
Kay Strobach <info@kay-strobach.de>
2024-01-10 16:16:35 +0100

    [BUGFIX] add missing migration
    Signed-off-by: Kay Strobach <info@kay-strobach.de>

---
+++ Migrations/Mysql/Version20240110151534.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Neos\Flow\Persistence\Doctrine\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240110151534 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MariaDb1027Platform,
+            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MariaDb1027Platform'."
+        );
+
+        $this->addSql('ALTER TABLE kaystrobach_tags_domain_model_tag ADD shorthand VARCHAR(255) NOT NULL, ADD iconidentifier VARCHAR(255) NOT NULL, ADD colorcode VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MariaDb1027Platform,
+            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MariaDb1027Platform'."
+        );
+
+        $this->addSql('ALTER TABLE kaystrobach_tags_domain_model_tag DROP shorthand, DROP iconidentifier, DROP colorcode');
+    }
+}
